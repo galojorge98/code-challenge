9a3ad341143d102229eec25db1906d1dd132e0ca
Galo Jorge <galojorge.gj@gmail.com>
2023-09-26 19:36:42 -0300

    terminado pero hay que ver cosas

--- javascript.js
+++ javascript.js
@@ -4,32 +4,36 @@ const dateOBform = document.getElementById("dateOfBirth");
 const button = document.getElementById("submit");
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    if(nameform.value == "" || lastnameform.value == "" || dateOBform.value == "") {
+    if (nameform.value == "" || lastnameform.value == "" || dateOBform.value == "") {
         alert("Asegurece de completar todo los campos");
     } else {
         alert("Formulario enviado con exito!!");
         console.log(`El nombes es: ${nameform.value} y apellidos es: ${lastnameform.value} y la fecha de nacimiento es: ${dateOBform.value}`);
-        const dataname = {
-            name: nameform.value
+        
+        4const dataname = {
+            id: 23,
+            name: nameform.value,
+            lastName: lastnameform.value,
+            dateOfBirt: dateOBform.value
         };
+        let headersList = {
+            "Accept": "*/*",
+            "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+            "Content-Type": "application/json"
+        }
+
+        let bodyContent = JSON.stringify(dataname);
+
+        let response = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "POST",
+            body: bodyContent,
+            headers: headersList
+        });
+
+        let data = await response.json();
+        console.log(data);
+
     }
 });
-
-const dataprueba = {
-    name:"galo",
-    id: 22 
-}
-const url = "https://jsonplaceholder.typicode.com/users";
-const datapost = {
-    headers:{
-        "content-type":"application.json; charset=UTF-8"
-    },
-    body: JSON.stringify(dataprueba),
-    method:"POST"
-};
-fetch(url, datapost)
- .then(res => res.json())
- .then(Response => {console.log(Response)})
- .catch(error =>console.log(error))
